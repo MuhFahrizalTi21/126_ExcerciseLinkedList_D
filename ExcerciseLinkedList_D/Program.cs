98e12feb70ef350f4ac1fb611d29b47a5cd71238
Muhammad  Fahrizal <m.fahrizal.ft21@mail.umy.ac.id>
2022-11-21 15:35:26 +0700

    Add project files.

---
+++ ExcerciseLinkedList_D/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcerciseLinkedList_D
+{
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
